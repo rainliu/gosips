4c20025dbfaa174988842af639a1519f4cb447fb
rainliu <yliu@live.com>
2013-04-27 12:07:56 -0700

    add ViaParser, but have bug in ContactParser

---
+++ parser/MaxForwardsParser_test.go
@@ -0,0 +1,18 @@
+package parser
+
+import (
+	"testing"
+)
+
+	func TestMaxForwardsParser(t *testing.T) {
+		var maxforwards =[]string{
+			"Max-Forwards: 35\n",
+			"Max-Forwards: 3495\n",
+			"Max-Forwards: 0 \n",
+        };
+
+		for i := 0; i < len(maxforwards); i++  {
+		    shp := NewMaxForwardsParser(maxforwards[i]);
+		    testHeaderParser(t, shp);
+		}
+	}
