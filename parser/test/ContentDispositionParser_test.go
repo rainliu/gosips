e43121fc88909602494acf652384c7cbbffe56c1
Rain Liu <yliu@live.com>
2014-04-20 16:27:41 -0700

    add and pass ContentDisposition

---
+++ parser/test/ContentDispositionParser_test.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestContentDispositionParser(t *testing.T) {
+	var contentDisposition = []string{
+		"Content-Disposition	:   session\n",
+		"Content-Disposition: render;   handling=hand;   optional=opt \n",
+	}
+
+	for i := 0; i < len(contentDisposition); i++ {
+		shp := NewContentDispositionParser(contentDisposition[i])
+		testHeaderParser(t, shp)
+	}
+}
+
+/** Test program
+  public static void main(String args[]) throws ParseException {
+      String r[] = {
+          "Content-Disposition: session\n",
+          "Content-Disposition: render;handling=hand;optional=opt \n"
+      };
+
+      for (int i = 0; i < r.length; i++ ) {
+          ContentDispositionParser parser =
+          new ContentDispositionParser(r[i]);
+          ContentDisposition cd= (ContentDisposition) parser.parse();
+          System.out.println("encoded = " + cd.encode());
+      }
+  }
+*/
