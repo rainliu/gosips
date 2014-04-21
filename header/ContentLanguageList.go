5ba1caaf291d68d7687d0b093cb3ce3ddd0c3b82
Rain Liu <yliu@live.com>
2014-04-20 17:07:19 -0700

    add and pass ContentLang

---
+++ header/ContentLanguageList.go
@@ -0,0 +1,21 @@
+package header
+
+import (
+	"gosip/core"
+)
+
+/**
+* ContentLanguage list of headers. (Should this be a list?)
+ */
+type ContentLanguageList struct {
+	SIPHeaderList
+}
+
+/** Default constructor
+ */
+
+func NewContentLanguageList() *ContentLanguageList {
+	this := &ContentLanguageList{}
+	this.SIPHeaderList.super(core.SIPHeaderNames_CONTENT_LANGUAGE)
+	return this
+}
---
+++ parser/test/ContentLanguageParser_test.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestContentLanguageParserParser(t *testing.T) {
+	var contentLanguage = []string{
+		"Content-Language: fr \n",
+		"Content-Language: fr , he \n",
+	}
+
+	for i := 0; i < len(contentLanguage); i++ {
+		shp := NewContentLanguageParser(contentLanguage[i])
+		testHeaderParser(t, shp)
+	}
+}
+
+/** Test program
+  public static void main(String args[]) throws ParseException {
+      String r[] = {
+          "Content-Language: fr \n",
+          "Content-Language: fr , he \n"
+      };
+
+      for (int i = 0; i < r.length; i++ ) {
+          ContentLanguageParser parser =
+          new ContentLanguageParser(r[i]);
+          ContentLanguageList e= (ContentLanguageList) parser.parse();
+          System.out.println("encoded = " + e.encode());
+      }
+  }
+*/
