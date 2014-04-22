6bb2f7c5c0c857650d819c05b25696a740a8ed9e
Rain Liu <yliu@live.com>
2014-04-21 21:49:42 -0700

    add and pass InReplyTo

---
+++ header/InReplyToList.go
@@ -0,0 +1,27 @@
+package header
+
+import (
+	"gosip/core"
+)
+
+/**
+* In-Reply-To SIP header. Keeps a list of CallIdentifiers
+*
+*@version  JAIN-SIP-1.1
+*
+*@author M. Ranganathan <mranga@nist.gov>  <br/>
+*
+*<a href="{@docRoot}/uncopyright.html">This code is in the public domain.</a>
+*
+ */
+type InReplyToList struct {
+	SIPHeaderList
+}
+
+/** Default constructor
+ */
+func NewInReplyToList() *InReplyToList {
+	this := &InReplyToList{}
+	this.SIPHeaderList.super(core.SIPHeaderNames_IN_REPLY_TO)
+	return this
+}
---
+++ parser/test/InReplyToParser_test.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestInReplyToParser(t *testing.T) {
+	var inReplyTo = []string{
+		"In-Reply-To: 70710@saturn.bell-tel.com, 17320@saturn.bell-tel.com\n",
+		"In-Reply-To: 70710 \n",
+	}
+
+	for i := 0; i < len(inReplyTo); i++ {
+		shp := NewInReplyToParser(inReplyTo[i])
+		testHeaderParser(t, shp)
+	}
+}
+
+/** Test program
+        public static void main(String args[]) throws ParseException {
+		String p[] = {
+                "In-Reply-To: 70710@saturn.bell-tel.com, 17320@saturn.bell-tel.com\n",
+                "In-Reply-To: 70710 \n"
+                };
+
+		for (int i = 0; i < p.length; i++ ) {
+		    InReplyToParser parser =
+			  new InReplyToParser(p[i]);
+		    InReplyToList in= (InReplyToList) parser.parse();
+		    System.out.println("encoded = " + in.encode());
+		}
+	}
+*/
