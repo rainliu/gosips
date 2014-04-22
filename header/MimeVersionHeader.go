b8dfb4694315fc2de16604ae0e2b750577aa4e44
Rain Liu <yliu@live.com>
2014-04-21 22:06:02 -0700

    add and pass MimeVersion

---
+++ header/MimeVersionHeader.go
@@ -0,0 +1,58 @@
+package header
+
+/**
+ * SIP messages MAY include a single MIME-Version general-header field to
+ * indicate what version of the MIME protocol was used to construct the
+ * message. Use of the MIME-Version header field indicates that the message is
+ * in full compliance with the MIME protocol as defined in
+ * <a href = "http://www.ietf.org/rfc/rfc2405.txt">RFC2045</a>. Proxies/gateways
+ * are responsible for ensuring full compliance (where possible) when exporting
+ * SIP messages to strict MIME environments.
+ * <p>
+ * For Example:<br>
+ * <code>MIME-Version: 1.0</code>
+ *
+ * @since 1.1
+ * @author Sun Microsystems
+ */
+type MimeVersionHeader interface {
+	Header
+
+	/**
+	 * Gets the Minor version value of this MimeVersionHeader.
+	 *
+	 * @return the Minor version of this MimeVersionHeader
+	 */
+	GetMinorVersion() int
+
+	/**
+	 * Sets the Minor-Version argument of this MimeVersionHeader to the supplied
+	 * minorVersion value.
+	 *
+	 * @param minorVersion - the new minor MIME version
+	 * @throws InvalidArgumentException if the supplied value is less than zero.
+	 */
+	SetMinorVersion(minorVersion int) //throws InvalidArgumentException;
+
+	/**
+	 * Gets the Major version value of this MimeVersionHeader.
+	 *
+	 * @return the Major version of this MimeVersionHeader
+	 */
+	GetMajorVersion() int
+
+	/**
+	 * Sets the Major-Version argument of this MimeVersionHeader to the supplied
+	 * majorVersion value.
+	 *
+	 * @param majorVersion - the new major MIME version
+	 * @throws InvalidArgumentException if the supplied version is less than zero.
+	 */
+	SetMajorVersion(majorVersion int) //throws InvalidArgumentException;
+
+	/**
+	 * Name of MimeVersionHeader
+	 */
+	//public final static String NAME = "MIME-Version";
+
+}
---
+++ parser/test/MimeVersionParser_test.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestMimeVersionParser(t *testing.T) {
+	var mimeVersion = []string{
+		"MIME-Version: 1.0 \n",
+	}
+
+	for i := 0; i < len(mimeVersion); i++ {
+		shp := NewMimeVersionParser(mimeVersion[i])
+		testHeaderParser(t, shp)
+	}
+}
+
+/** Test program
+        public static void main(String args[]) throws ParseException {
+		String r[] = {
+                "MIME-Version: 1.0 \n"
+                };
+
+		for (int i = 0; i < r.length; i++ ) {
+		    MimeVersionParser parser =
+			  new MimeVersionParser(r[i]);
+		    MimeVersion m= (MimeVersion) parser.parse();
+		    System.out.println("encoded = " +m.encode());
+		}
+	}
+*/
