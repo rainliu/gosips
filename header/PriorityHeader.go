a8c50aed6777833c90b5fbaf095b4a9597e70bd8
Rain Liu <yliu@live.com>
2014-04-22 20:33:58 -0700

    add and pass Priority

---
+++ header/PriorityHeader.go
@@ -0,0 +1,132 @@
+package header
+
+/**
+
+ * The Priority header field indicates the urgency of the request as perceived
+
+ * by the client.  The Priority header field describes the priority that the
+
+ * SIP request should have to the receiving human or its agent. For example,
+
+ * it may be factored into decisions about call routing and acceptance. For
+
+ * these decisions, a message containing no Priority header field SHOULD be
+
+ * treated as if it specified a Priority of <var>"Normal"</var>.
+
+ * <p>
+
+ * The Priority header field does not influence the use of communications
+
+ * resources such as packet forwarding priority in routers or access to
+
+ * circuits in PSTN gateways.
+
+ * <p>
+
+ * The currently defined priority values are:
+
+ * <ul>
+
+ * <li> EMERGENCY
+
+ * <li> URGENT
+
+ * <li> NORMAL
+
+ * <li> NON_URGENT
+
+ * </ul>
+
+ * For Example:<br>
+
+ * <code>Subject: Weekend plans<br>
+
+ * Priority: non-urgent</code>
+
+ *
+
+ * @version 1.1
+
+ * @author Sun Microsystems
+
+ */
+
+type PriorityHeader interface {
+	Header
+
+	/**
+
+	 * Set priority of PriorityHeader
+
+	 *
+
+	 * @param priority - the new string priority value
+
+	 * @throws ParseException which signals that an error has been reached
+
+	 * unexpectedly while parsing the priority value.
+
+	 */
+
+	SetPriority(priority string) //throws ParseException;
+
+	/**
+
+	 * Gets the string priority value of the PriorityHeader.
+
+	 *
+
+	 * @return the string priority value of the PriorityHeader
+
+	 */
+
+	GetPriority() string
+
+	/**
+
+	 * Urgent priority constant
+
+	 */
+
+	//public static final String URGENT = "Urgent";
+
+	/**
+
+	 * Normal priority constant
+
+	 */
+
+	//public static final String NORMAL = "Normal";
+
+	/**
+
+	 * Non-urgent priority constant
+
+	 */
+
+	//public static final String NON_URGENT = "Non-Urgent";
+
+	/**
+
+	 * Emergency priority constant - It is RECOMMENDED that the value of
+
+	 * "emergency" only be used when life, limb, or property are in imminent
+
+	 * danger. Otherwise, there are no semantics defined for this header
+
+	 * field.
+
+	 */
+
+	//public static final String EMERGENCY = "Emergency";
+
+	/**
+
+	 * Name of PriorityHeader
+
+	 */
+
+	//public final static String NAME = "Priority";
+
+}
---
+++ parser/PriorityParser_test.go
@@ -0,0 +1,31 @@
+package parser
+
+import (
+	"testing"
+)
+
+func TestPriorityParser(t *testing.T) {
+	var inputs = []string{
+		"Priority: emergency\n",
+	}
+
+	for i := 0; i < len(inputs); i++ {
+		shp := NewPriorityParser(inputs[i])
+		testHeaderParser(t, shp)
+	}
+}
+
+/** Test program
+        public static void main(String args[]) throws ParseException {
+		String p[] = {
+                "Priority: emergency\n"
+                };
+
+		for (int i = 0; i < p.length; i++ ) {
+		    PriorityParser parser =
+			  new PriorityParser(p[i]);
+		    Priority prio= (Priority) parser.parse();
+		    System.out.println("encoded = " + prio.encode());
+		}
+	}
+*/
