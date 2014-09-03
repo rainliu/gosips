cf6a3cfbe027a4cc87ae843d38ca14224dead479
Rain Liu <yuliu@outlook.com>
2014-09-02 23:00:54 -0700

    add rtcp bye packet

--- rtp/rtcpbyepacket.go
+++ rtp/rtcpbyepacket.go
@@ -1,133 +1,134 @@
 package rtp
 
+import "fmt"
+
 /** Describes an RTCP BYE packet. */
 type RTCPBYEPacket struct {
 	RTCPPacket
 	reasonoffset int
 }
 
-// 	/** Creates an instance based on the data in \c data with length \c datalen.
-// 	 *  Creates an instance based on the data in \c data with length \c datalen. Since the \c data pointer
-// 	 *  is referenced inside the class (no copy of the data is made) one must make sure that the memory it
-// 	 *  points to is valid as long as the class instance exists.
-// 	 */
-// RTCPBYEPacket::RTCPBYEPacket(uint8_t *data,size_t datalength)
-// 	: RTCPPacket(BYE,data,datalength)
-// {
-// 	knownformat = false;
-// 	reasonoffset = 0;
+/** Creates an instance based on the data in \c data with length \c datalen.
+ *  Creates an instance based on the data in \c data with length \c datalen. Since the \c data pointer
+ *  is referenced inside the class (no copy of the data is made) one must make sure that the memory it
+ *  points to is valid as long as the class instance exists.
+ */
+func NewRTCPBYEPacket(data []byte, datalen int) *RTCPBYEPacket {
+	this := &RTCPBYEPacket{}
+	this.data = make([]byte, datalen)
+	this.datalen = datalen
+	copy(this.data[:], data[0:datalen])
+	this.packettype = BYE
+	this.knownformat = false
 
-// 	RTCPCommonHeader *hdr;
-// 	size_t len = datalength;
+	this.reasonoffset = 0
+	byelen := datalen
+	if ((this.data[0] >> RTCP_HEADER_P_POS) & RTCP_HEADER_P_MSK) != 0 {
+		padcount := data[datalen-1]
+		if (padcount & 0x03) != 0 { // not a multiple of four! (see rfc 3550 p 37)
+			return this
+		}
+		if int(padcount) >= byelen {
+			return this
+		}
+		byelen -= int(padcount)
+	}
 
-// 	hdr = (RTCPCommonHeader *)data;
-// 	if (hdr->padding)
-// 	{
-// 		uint8_t padcount = data[datalength-1];
-// 		if ((padcount & 0x03) != 0) // not a multiple of four! (see rfc 3550 p 37)
-// 			return;
-// 		if (((size_t)padcount) >= len)
-// 			return;
-// 		len -= (size_t)padcount;
-// 	}
+	count := (this.data[0] >> RTCP_HEADER_C_POS) & RTCP_HEADER_C_MSK
+	ssrclen := SIZEOF_RTCPHEADER + int(count)*4 //*sizeof(uint32_t) + sizeof(RTCPCommonHeader);
+	if ssrclen > byelen {
+		return this
+	}
+	if ssrclen < byelen { // there's probably a reason for leaving
+		reaslen := int(data[ssrclen])
+		if reaslen > byelen-ssrclen-1 {
+			return this
+		}
+		this.reasonoffset = ssrclen
+	}
+	this.knownformat = true
 
-// 	size_t ssrclen = ((size_t)(hdr->count))*sizeof(uint32_t) + sizeof(RTCPCommonHeader);
-// 	if (ssrclen > len)
-// 		return;
-// 	if (ssrclen < len) // there's probably a reason for leaving
-// 	{
-// 		uint8_t *reasonlength = (data+ssrclen);
-// 		size_t reaslen = (size_t)(*reasonlength);
-// 		if (reaslen > (len-ssrclen-1))
-// 			return;
-// 		reasonoffset = ssrclen;
-// 	}
-// 	knownformat = true;
-// }
-// 	/** Returns the number of SSRC identifiers present in this BYE packet. */
-// 	int GetSSRCCount() const;
+	return this
+}
 
-// 	/** Returns the SSRC described by \c index which may have a value from 0 to GetSSRCCount()-1
-// 	 *  (note that no check is performed to see if \c index is valid).
-// 	 */
-// 	uint32_t GetSSRC(int index) const; // note: no check is performed to see if index is valid!
+/** Returns the number of SSRC identifiers present in this BYE packet. */
+func (this *RTCPBYEPacket) GetSSRCCount() int {
+	if !this.knownformat {
+		return 0
+	}
 
-// 	/** Returns true if the BYE packet contains a reason for leaving. */
-// 	bool HasReasonForLeaving() const;
+	count := (this.data[0] >> RTCP_HEADER_C_POS) & RTCP_HEADER_C_MSK
 
-// 	/** Returns the length of the string which describes why the source(s) left. */
-// 	size_t GetReasonLength() const;
+	//RTCPCommonHeader *hdr = (RTCPCommonHeader *)data;
+	return int(count) //(int)(hdr->count);
+}
 
-// 	/** Returns the actual reason for leaving data. */
-// 	uint8_t *GetReasonData();
+/** Returns the SSRC described by \c index which may have a value from 0 to GetSSRCCount()-1
+ *  (note that no check is performed to see if \c index is valid).
+ */
+func (this *RTCPBYEPacket) GetSSRC(index int) uint32 {
+	if !this.knownformat {
+		return 0
+	}
+	ssrc := uint32(this.data[SIZEOF_RTCPHEADER+4*index+0])<<24 |
+		uint32(this.data[SIZEOF_RTCPHEADER+4*index+1])<<16 |
+		uint32(this.data[SIZEOF_RTCPHEADER+4*index+2])<<8 |
+		uint32(this.data[SIZEOF_RTCPHEADER+4*index+3])<<0
+	//uint32_t *ssrc = (uint32_t *)(data+sizeof(RTCPCommonHeader)+sizeof(uint32_t)*index);
+	return ssrc //ntohl(*ssrc);
+}
 
-// inline int RTCPBYEPacket::GetSSRCCount() const
-// {
-// 	if (!knownformat)
-// 		return 0;
+/** Returns true if the BYE packet contains a reason for leaving. */
+func (this *RTCPBYEPacket) HasReasonForLeaving() bool {
+	if !this.knownformat {
+		return false
+	}
+	if this.reasonoffset == 0 {
+		return false
+	}
+	return true
+}
 
-// 	RTCPCommonHeader *hdr = (RTCPCommonHeader *)data;
-// 	return (int)(hdr->count);
-// }
+/** Returns the length of the string which describes why the source(s) left. */
+func (this *RTCPBYEPacket) GetReasonLength() int {
+	if !this.knownformat {
+		return 0
+	}
+	if this.reasonoffset == 0 {
+		return 0
+	}
+	reasonlen := int(this.data[this.reasonoffset])
+	return reasonlen
+}
 
-// inline uint32_t RTCPBYEPacket::GetSSRC(int index) const
-// {
-// 	if (!knownformat)
-// 		return 0;
-// 	uint32_t *ssrc = (uint32_t *)(data+sizeof(RTCPCommonHeader)+sizeof(uint32_t)*index);
-// 	return ntohl(*ssrc);
-// }
+/** Returns the actual reason for leaving data. */
+func (this *RTCPBYEPacket) GetReasonData() []byte {
+	if !this.knownformat {
+		return nil
+	}
+	if this.reasonoffset == 0 {
+		return nil
+	}
+	reasonlen := this.data[this.reasonoffset]
+	if reasonlen == 0 {
+		return nil
+	}
+	return this.data[this.reasonoffset+1:]
+}
 
-// inline bool RTCPBYEPacket::HasReasonForLeaving() const
-// {
-// 	if (!knownformat)
-// 		return false;
-// 	if (reasonoffset == 0)
-// 		return false;
-// 	return true;
-// }
+func (this *RTCPBYEPacket) Dump() {
+	this.RTCPPacket.Dump()
+	if !this.IsKnownFormat() {
+		fmt.Printf("    Unknown format")
+		return
+	}
 
-// inline size_t RTCPBYEPacket::GetReasonLength() const
-// {
-// 	if (!knownformat)
-// 		return 0;
-// 	if (reasonoffset == 0)
-// 		return 0;
-// 	uint8_t *reasonlen = (data+reasonoffset);
-// 	return (size_t)(*reasonlen);
-// }
+	num := this.GetSSRCCount()
 
-// inline uint8_t *RTCPBYEPacket::GetReasonData()
-// {
-// 	if (!knownformat)
-// 		return 0;
-// 	if (reasonoffset == 0)
-// 		return 0;
-// 	uint8_t *reasonlen = (data+reasonoffset);
-// 	if ((*reasonlen) == 0)
-// 		return 0;
-// 	return (data+reasonoffset+1);
-// }
-
-// void RTCPBYEPacket::Dump()
-// {
-// 	RTCPPacket::Dump();
-// 	if (!IsKnownFormat())
-// 	{
-// 		std::cout << "    Unknown format" << std::endl;
-// 		return;
-// 	}
-
-// 	int num = GetSSRCCount();
-// 	int i;
-
-// 	for (i = 0 ; i < num ; i++)
-// 		std::cout << "    SSRC: " << GetSSRC(i) << std::endl;
-// 	if (HasReasonForLeaving())
-// 	{
-// 		char str[1024];
-// 		memcpy(str,GetReasonData(),GetReasonLength());
-// 		str[GetReasonLength()] = 0;
-// 		std::cout << "    Reason: " << str << std::endl;
-// 	}
-// }
+	for i := 0; i < num; i++ {
+		fmt.Printf("    SSRC: %d", this.GetSSRC(i))
+	}
+	if this.HasReasonForLeaving() {
+		fmt.Printf("    Reason: %s", this.GetReasonData())
+	}
+}
